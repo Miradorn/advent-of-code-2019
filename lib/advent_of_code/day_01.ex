2e6335e2a9060b1d79250ae4c8a5c690c8a653aa
Alexander Schramm <alex@schram.me>
2019-12-02 12:46:12 +0100

    day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,23 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    args
+    |> Enum.map(fn mass ->
+      Float.floor(mass / 3) - 2
+    end)
+    |> Enum.reduce(&+/2)
   end
 
   def part2(args) do
+    args
+    |> Enum.map(&fuel/1)
+    |> Enum.reduce(&+/2)
+  end
+
+  defp fuel(mass) do
+    (Float.floor(mass / 3) - 2)
+    |> case do
+      mass when mass > 0 -> mass + fuel(mass)
+      mass -> 0
+    end
   end
 end
