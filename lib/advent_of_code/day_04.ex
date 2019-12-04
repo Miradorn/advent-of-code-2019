9746c29dacae44462f21575002e16f971d99e339
Alexander Schramm <alex@schram.me>
2019-12-04 09:05:01 +0100

    d04

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,33 @@
 defmodule AdventOfCode.Day04 do
   def part1(args) do
+    args
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.count(&test1/1)
+  end
+
+  defp test1(<<a, b, c, d, e, f>>) do
+    no_decrease = a <= b and b <= c and c <= d and d <= e and e <= f
+    adjacent_equal = a == b or b == c or c == d or d == e or e == f
+
+    no_decrease and adjacent_equal
   end
 
   def part2(args) do
+    args
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.count(&test2/1)
+  end
+
+  defp test2(<<a, b, c, d, e, f>>) do
+    no_decrease = a <= b and b <= c and c <= d and d <= e and e <= f
+
+    adjacent_equal =
+      (a == b and not (b == c)) or
+        (b == c and not (a == b or c == d)) or
+        (c == d and not (b == c or d == e)) or
+        (d == e and not (c == d or e == f)) or
+        (e == f and not (d == e))
+
+    no_decrease and adjacent_equal
   end
 end
--- lib/mix/tasks/d04.p2.ex
+++ lib/mix/tasks/d04.p2.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.D04.P2 do
 
   @shortdoc "Day 04 Part 2"
   def run(args) do
-    input = nil
+    input = 284_639..748_759
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
